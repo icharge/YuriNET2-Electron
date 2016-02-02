f1eab75c3472f386c8f65892e78db23a2744f62d
iCharge <charge_n@hotmail.com>
2016-02-03 00:15:05 +0700

    Add state debug info

--- public/js/app.js
+++ public/js/app.js
@@ -78,6 +78,22 @@ ScriptManager
 /**
  * Root scope
  */
-app.run(['$rootScope', function ($rootScope) {
+app.run(['$rootScope', '$state', '$stateParams', 'Translator',
+    function ($rootScope, $state, $stateParams, Translator) {
 
-}]);
+        // Change title to Application name with version
+        $rootScope.$on('$translateChangeSuccess', function (e, data) {
+            window.document.title = Translator.getText('APP_NAME') + ' (Alpha)';
+        });
+
+
+        /**
+         It's very handy to add references to $state and $stateParams to the $rootScope
+         so that you can access them from any scope within your applications.For example,
+         <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
+         to active whenever 'contacts.list' or one of its decendents is active.
+         */
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+    }
+]);

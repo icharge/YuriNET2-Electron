971ec3031f79077f3b7d5afd70dc1df71b42d7b9
Norrapat Nimmanee <norrapat@genesiscorp.tech>
2018-08-27 21:53:10 +0700

    clean up test

--- src/test.ts
+++ src/test.ts
@@ -6,9 +6,6 @@ import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
-import { Observable } from 'rxjs/Observable';
-import { TranslateLoader, TranslateService } from '@ngx-translate/core';
-
 declare const require: any;
 
 // First, initialize the Angular testing environment.
